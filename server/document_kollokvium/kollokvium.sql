996cbed722e0b4ebe61d8513b91db78f87158696
Slip7356 <10thegt10@gmail.com>
2023-04-14 11:02:48 +0200

    adatbázis módosítása

--- server/document_kollokvium/kollokvium.sql
+++ server/document_kollokvium/kollokvium.sql
@@ -1,3 +1,30 @@
 select * from hallgato;
 SELECT * from jelentkezes;
 SELECT * from vizsga;
+
+#user tábla létrehozása
+  CREATE TABLE IF NOT EXISTS users (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  firstName VARCHAR(50) DEFAULT NULL,
+  lastName VARCHAR(50) DEFAULT NULL,
+  gender VARCHAR(10) DEFAULT NULL,
+  userName VARCHAR(50) DEFAULT NULL,
+  email VARCHAR(255) DEFAULT NULL,
+  password VARCHAR(100) DEFAULT NULL,
+  number INT(11) DEFAULT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+AUTO_INCREMENT = 24,
+AVG_ROW_LENGTH = 5461,
+CHARACTER SET utf8,
+COLLATE utf8_hungarian_ci;
+
+#Átalakítás
+select SUBSTRING_INDEX('Kiss Béla', ' ', 1) veznev,SUBSTRING_INDEX('Kiss Béla', ' ', -1) kernev ;
+
+#Email
+SELECT concat(veznev('Kiss Béla'),'.', kernev('Kiss Béla'),'@gmail.com');
+SELECT email ('Kiss Béla');
+SELECT nev,COUNT(*) db from users 
+group by nev;
--- server/serverAuth.js
+++ server/serverAuth.js
@@ -5,13 +5,22 @@ const app = express();
 const jwt = require("jsonwebtoken");
 const pool = require("./config/database.js");
 const { genSaltSync, hashSync, compareSync } = require("bcrypt");
-
+const cors = require("cors");
 const {
   sendingInfo
 } = require("./config/sending.js");
 
-
+//#region Middleware
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*", //http://localhost:8080
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+  );
+  //#endregion Middleware
+  
 //itt tároljuk a refrest tokeneket
 refreshTokens = [];
 
@@ -31,6 +40,9 @@ app.post("/login", (req, res) => {
         message: "Invalid username or password",
         accessToken: "",
         refreshToken: "",
+        userId: 0, 
+        number: 0,
+        accessTime: "0"
       });
     }
 
@@ -48,14 +60,27 @@ app.post("/login", (req, res) => {
       refreshTokens.push(refreshToken);
 
       //mindkét tokent odaadjuk a bejelentkezőnek
-      sendingInfo(res, 1, "login successfully", { accessToken: accessToken, refreshToken: refreshToken }, 200);
+      sendingInfo(res, 1, "login successfully", { 
+        accessToken: accessToken, 
+        refreshToken: refreshToken,
+        userName: results.userName,
+        userId: results.id, 
+        number: results.number,
+        accessTime: process.env.ACCESS_TIME
+      }, 200);
 
       console.log("accessToken /login:", accessToken);
       console.log("refreshToken /login:", refreshToken);
       console.log("refreshTokens /login:", refreshTokens);
       return;
     } else {
-      sendingInfo(res, 1, "Invalid username or password", { accessToken: "", refreshToken: "" }, 200);
+      sendingInfo(res, 1, "Invalid username or password", { 
+        accessToken: "", 
+        refreshToken: "",
+        userId: 0, 
+        number: 0,
+        accessTime: "0"
+      }, 200);
       return;
     }
   });
